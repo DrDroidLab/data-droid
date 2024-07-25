54aeb8f4a73f11c86e89499ae1b18bea3460fac2
Karanveer Singh Sirohi <karanveersinghsirohi@Siddarths-MacBook-Pro-2.local>
2024-07-25 18:23:49 +0530

    added kubectl_processor

--- client/factory.py
+++ client/factory.py
@@ -1,5 +1,6 @@
 from source_processors.aws_cloudwatch_processor import AWSCloudwatchProcessor
 from source_processors.bash_processor import BashProcessor
+from source_processors.kubectl_processor import KubectlProcessor
 
 
 class ClientFactory:
@@ -11,3 +12,7 @@ class ClientFactory:
     @staticmethod
     def get_aws_cloudwatch_client(client_type: str, region: str, aws_access_key: str, aws_secret_key: str):
         return AWSCloudwatchProcessor(client_type, region, aws_access_key, aws_secret_key)
+
+    @staticmethod
+    def get_kubectl_client(api_server: str, token: str, ssl_ca_cert: str = None, ssl_ca_cert_path: str = None):
+        return KubectlProcessor(api_server, token, ssl_ca_cert, ssl_ca_cert_path)
